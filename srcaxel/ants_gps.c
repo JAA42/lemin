94d949871abbceea363faeba10e00524b74da5c9
adhondt42 <adhondt@student.42.fr>
2018-06-15 18:38:57 +0200

    autocommit

--- srcaxel/ants_gps.c
+++ srcaxel/ants_gps.c
@@ -2,12 +2,23 @@
 
 void	send_ants(w_pm *w, int checkpoint_sum)
 {
-	while (w->ants_count - checkpoint_sum >= 0)
+	while (//fourmis 1 n'est pas a end)
 	{
-		// lancer algo 1
+	// si i == 1 afficher une fois 
+	// i == 2 afficher deux fois 
+	//			si afficher 2 fois, 
+	//				tant que (nbr de fois q qfficher > 0 & nbr de fois == 2)
+	//					afficher Lx-yeme element de way
+	//					x++; y--;
+	//	i == 3 
+	//		afficher 3 fourmis, nb de fois a aficher == 3 
+	//			tant que (nbr de fois a afficher > 0)
+	//				afficher Lx-yeme element de way 
+	//				x++, y--;
+	//etc....
 	}
-	while (w->ants_count >= 0)
+	while (//fourmis 1 à end, construire làlgo en sens inverse)
 	{
-		// lancer algo 2
+		// algo dans le sens inverse 
 	}
 }
